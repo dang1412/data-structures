ee59846cd42364f5ea56a5911602e8ba5c5791a3
Dang Thanh Tung <dttung1412@gmail.com>
2021-02-19 18:58:58 +0900

    feat(binarytree): insert update

--- src/ts/binarytree/binarytree.ts
+++ src/ts/binarytree/binarytree.ts
@@ -1,9 +1,3 @@
-// interface BinaryTree {
-//   data: number
-//   left?: BinaryTree
-//   right?: BinaryTree
-// }
-
 export class BinaryNode {
   left: BinaryNode | null = null
   right: BinaryNode | null = null
@@ -19,10 +13,78 @@ export class BinaryNode {
 
     return next === null ? null : next.search(data)
   }
+
+  insert(data: number): boolean {
+    if (data === this.data) {
+      return false
+    }
+
+    const isGoLeft = data < this.data
+
+    const next = isGoLeft ? this.left : this.right
+
+    if (next === null) {
+      const newNode = new BinaryNode(data)
+      if (isGoLeft) {
+        this.left = newNode
+      } else {
+        this.right = newNode
+      }
+
+      return true
+    }
+
+    return next.insert(data)
+  }
+
+  delete(data: number): BinaryNode | null {
+    if (data < this.data) {
+      if (this.left) this.left = this.left.delete(data)
+    } else if (data > this.data) {
+      if (this.right) this.right = this.right.delete(data)
+    } else {
+      if (this.left === null) {
+        return this.right
+      }
+
+      if (this.right === null) {
+        return this.left
+      }
+
+      // replace with min value of right subtree
+      const nextVal = this.right.minValue()
+      this.data = nextVal
+
+      // delete min value of right subtree
+      this.right = this.right.delete(nextVal)
+    }
+
+    return this
+  }
+
+  minValue(): number {
+    let node = this as BinaryNode
+    while (node.left) node = node.left
+
+    return node.data
+  }
 }
 
 export class BinaryTree {
   root: BinaryNode | null = null
+
+  search(data: number): BinaryNode | null {
+    return this.root ? this.root.search(data): null
+  }
+
+  insert(data: number): boolean {
+    if (!this.root) {
+      this.root = new BinaryNode(data)
+      return true
+    }
+
+    return this.root.insert(data)
+  }
 }
 
 // export function search(t: BinaryTree | null, data: number): BinaryTree | null {
